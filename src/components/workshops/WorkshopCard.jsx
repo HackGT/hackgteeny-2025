c4a1a38907aff6dd294a16c4dc37da04897cb74d
ganning127 <ganningxu@gmail.com>
2025-01-22 16:23:54 -0500

    added mobile responsive fixes

--- src/components/workshops/WorkshopCard.jsx
+++ src/components/workshops/WorkshopCard.jsx
@@ -1,32 +1,34 @@
 import { Box, Image, Text } from '@chakra-ui/react';
 import styleConstants from '../../styleConstants';
 
-const WorkshopCard = ({title, icon, children}) => {
+const WorkshopCard = ({ title, icon, children }) => {
 	return (
 		<Box
-		gap={4}
-		display={{base: 'flex', lg: 'block'}}
-		alignItems="center">
-		
-			<Image 
-			alignSelf={{base: 'flex-start', lg: 'center'}}
-			width={{base: "30vw", lg: "unset"}}
-			src={icon} 
-			alt={title} 
-			mb={26} />
-			
+			textAlign={{ base: 'center', lg: 'left' }}
+			gap={4}
+			alignItems="center">
+
+			<Image
+				alignSelf={{ base: 'flex-start', lg: 'center' }}
+				width={{ base: "30vw", lg: "unset" }}
+				src={icon}
+				alt={title}
+				mx={{ base: 'auto', lg: '0' }}
+				mb={26} />
+
 			<Box
-			display='flex'
-			flexDir='column'>
-				<Text 
-				mb={26}
-				fontFamily={styleConstants.fonts.subheading} 
-				fontSize='22.8px' 
-				fontWeight='500'>
+				display='flex'
+				flexDir='column'>
+				<Text
+					mb={26}
+					textAlign='center'
+					fontFamily={styleConstants.fonts.subheading}
+					fontSize={{ base: '20px', lg: '27px' }}
+					fontWeight='500'>
 					{title}
 				</Text>
 
-				<Box fontSize='11.4px'>
+				<Box fontSize={{ base: '16px', lg: '18px' }}>
 					{children}
 				</Box>
 			</Box>
