4b0669a5ed69a39f3fdcf0d357be440b1ec48767
ganning127 <ganningxu@gmail.com>
2025-02-03 01:13:33 -0500

    add schedule details

--- src/components/schedule/ScheduleCard.jsx
+++ src/components/schedule/ScheduleCard.jsx
@@ -1,48 +1,38 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, SimpleGrid, Text } from '@chakra-ui/react';
 import React from 'react';
 import styleConstants from '../../styleConstants';
 
 const ScheduleCard = ({ color, title, children }) => {
 	return (
-		<Box
-			display={{ base: 'flex', lg: 'grid' }}
-			gap={{ base: 2, lg: 10 }}
-			gridTemplateColumns={{ base: '1fr', lg: 'auto auto' }}
-			flexDir="column"
+		<SimpleGrid
+			columns={{ base: 1, lg: 2 }}
+			gap={2}
+			alignItems='center'
 		>
 			{/* Left Column - Time */}
-			<Box
-				display="flex"
-				flexDir="column"
-				alignItems={{ base: 'center', lg: 'flex-start' }}
+			<Text
+				bgColor={color}
+				borderRadius={styleConstants.borderRadius}
+				color="white"
+				fontSize={{ base: "18px", lg: "20px" }}
+				fontWeight={400}
+				p="0.6rem"
+				w={{ base: "fit-content", lg: "250px" }}
+				mx={{ base: "auto", lg: 0 }}
+				textAlign='center'
+				justifySelf="flex-end"
 			>
-				<Text
-					bgColor={color}
-					borderRadius={styleConstants.borderRadius}
-					color="white"
-					fontSize={{ base: "18px", lg: "20px" }}
-					fontWeight={400}
-					p="0.6rem min(10vw, 4rem)"
-					w={{ base: "fit-content", lg: "190px" }}
-					textAlign={{ base: 'center', lg: 'flex-start' }}
-					h="fit-content"
-				>
-					{title}
-				</Text>
-			</Box>
+				{title}
+			</Text>
 
 			{/* Right Column - Description */}
 			<Box
 				fontSize={{ base: "16px", lg: "20px" }}
 				textAlign={{ base: 'center', lg: 'left' }}
-				display="flex"
-				flexDir="column"
-				alignItems={{ base: 'center', lg: 'flex-start' }}
-				justifyContent="center"
 			>
 				{children}
 			</Box>
-		</Box>
+		</SimpleGrid>
 	);
 };
 
--- src/components/schedule/ScheduleSection.jsx
+++ src/components/schedule/ScheduleSection.jsx
@@ -1,15 +1,84 @@
 import React from 'react';
 import GenericSection from '../GenericSection';
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import ScheduleCard from './ScheduleCard';
+
+/*
+9:30 AM to 10:30 AM: Checkin —> Outside Klaus 1443
+10:30 AM to 11:00 AM: Opening Ceremony —> Klaus 1443
+11AM to 12PM: Al @ GT workshop —> Klaus 2443
+12PM to 1pm: Lunch (pizza) —> Klaus 1443
+1PM to 2PM: Design Club Workshop —> Klaus 1443
+2PM to 3PM: IOS and GitMad (choose 1) —> IOS at Klaus 2443, GitMad at Klaus 1443
+3 PM to 3:30PM: BREAK
+3:30PM to 4:30PM: Startup Exchange Big Tech Speaker —> Klaus 2443
+4:30PM to 5PM: Closing Ceremony, Hand out Shirts —> Klaus
+*/
+
+const SCHEDULE_ITEMS = [
+	{
+		time: "9:30 - 10:30 AM",
+		description: "Check In: Outside Klaus 1443",
+		color: "#33c2ff"
+	},
+	{
+		time: "10:30 - 11:00 AM",
+		description: "Opening Ceremony: Klaus 1443",
+		color: "#29b9ff"
+	},
+	{
+		time: "11:00 AM - 12:00 PM",
+		description: "Al @ GT Workshop: Klaus 2443",
+		color: "#28afff"
+	},
+	{
+		time: "12:00 - 1:00 PM",
+		description: "Lunch (Pizza): Klaus 1443",
+		color: "#30a5ff"
+	},
+	{
+		time: "1:00 - 2:00 PM",
+		description: "Design Club Workshop: Klaus 1443",
+		color: "#3e9bff"
+	},
+	{
+		time: "2:00 - 3:00 PM",
+		description: "IOS and GitMad (Choose 1): IOS at Klaus 2443, GitMad at Klaus 1443",
+		color: "#4d90ff"
+	},
+	{
+		time: "3:00 - 3:30 PM",
+		description: "BREAK",
+		color: "#5d84fb"
+	},
+	{
+		time: "3:30 - 4:30 PM",
+		description: "Startup Exchange Big Tech Speaker: Klaus 2443",
+		color: "#6c77f5"
+	},
+	{
+		time: "4:30 - 5:00 PM",
+		description: "Closing Ceremony: Klaus 1443",
+		color: "#7b69ec"
+	}
+];
 
 const ScheduleSection = () => {
 	return (
 		<GenericSection id="schedule" title="Schedule">
-			<Text fontSize='xl' align='center'>We&apos;re working on it, coming soon!</Text>
-			{/* <Flex flexDir="column" gap={16}>
+			<Flex flexDir="column" gap={16}>
 
-				<ScheduleCard
+				{
+					SCHEDULE_ITEMS.map((item, index) => {
+						return (
+							<ScheduleCard key={index} title={item.time} color={item.color}>
+								{item.description}
+							</ScheduleCard>
+						);
+					})
+				}
+
+				{/* <ScheduleCard
 					title={"10 am"} color={"#33C2FF"}>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 				</ScheduleCard>
@@ -27,9 +96,9 @@ const ScheduleSection = () => {
 				<ScheduleCard
 					title={"10 am"} color={"#7B69EC"}>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-				</ScheduleCard>
+				</ScheduleCard> */}
 
-			</Flex> */}
+			</Flex>
 		</GenericSection>
 	);
 };
