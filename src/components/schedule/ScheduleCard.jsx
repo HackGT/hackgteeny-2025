ee5181e686c51147822c3dc9adc9a238bb91a81d
ganning127 <ganningxu@gmail.com>
2025-02-06 17:05:26 -0500

    add space in schedule

--- src/components/schedule/ScheduleCard.jsx
+++ src/components/schedule/ScheduleCard.jsx
@@ -3,37 +3,37 @@ import React from 'react';
 import styleConstants from '../../styleConstants';
 
 const ScheduleCard = ({ color, title, children }) => {
-	return (
-		<SimpleGrid
-			columns={{ base: 1, lg: 2 }}
-			gap={2}
-			alignItems='center'
-		>
-			{/* Left Column - Time */}
-			<Text
-				bgColor={color}
-				borderRadius={styleConstants.borderRadius}
-				color="white"
-				fontSize={{ base: "18px", lg: "20px" }}
-				fontWeight={400}
-				p="0.6rem"
-				w={{ base: "fit-content", lg: "250px" }}
-				mx={{ base: "auto", lg: 0 }}
-				textAlign='center'
-				justifySelf="flex-end"
-			>
-				{title}
-			</Text>
+  return (
+    <SimpleGrid
+      columns={{ base: 1, lg: 2 }}
+      gap={{ base: 2, lg: 16 }}
+      alignItems='center'
+    >
+      {/* Left Column - Time */}
+      <Text
+        bgColor={color}
+        borderRadius={styleConstants.borderRadius}
+        color="white"
+        fontSize={{ base: "18px", lg: "20px" }}
+        fontWeight={400}
+        p="0.6rem"
+        w={{ base: "fit-content", lg: "250px" }}
+        mx={{ base: "auto", lg: 0 }}
+        textAlign='center'
+        justifySelf="flex-end"
+      >
+        {title}
+      </Text>
 
-			{/* Right Column - Description */}
-			<Box
-				fontSize={{ base: "16px", lg: "20px" }}
-				textAlign={{ base: 'center', lg: 'left' }}
-			>
-				{children}
-			</Box>
-		</SimpleGrid>
-	);
+      {/* Right Column - Description */}
+      <Box
+        fontSize={{ base: "16px", lg: "20px" }}
+        textAlign={{ base: 'center', lg: 'left' }}
+      >
+        {children}
+      </Box>
+    </SimpleGrid>
+  );
 };
 
 export default ScheduleCard;
